0ef9e0d438d8ebafb4045faabcf22f42ea0c0a5d
aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-09-11 12:42:57 +0530

    sdgdfhd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { IoReorderThreeOutline } from "react-icons/io5";
+import { FaCaretDown } from "react-icons/fa";
 
 
 function App() {
   return (
-    <div className='mt-7'>
-       <div className='flex'>
-      <div className='font-bolder text-3xl'style={{ color: 'rgb(130, 124, 161)' }} ><IoReorderThreeOutline /></div>
-        <div className='text-3xl bg-slate-200 rounded-full w-[50px] h-[50px] text-center ' style={{ color: 'rgb(130, 124, 161)' }}>AK</div>
+    <div className='mt-7 px-3'>
+       <div className='flex justify-between'>
+      <div className='font-bolder text-[50px]'style={{ color: 'rgb(130, 124, 161)' }} ><IoReorderThreeOutline /></div>
+       <div className='flex items-center gap-2'>
+                <div className='text-3xl font-thin bg-slate-200 rounded-full w-[50px] h-[50px] text-center ' style={{ color: 'rgb(130, 124, 161)' }}>AK</div>
+<div className='2xl 'style={{ color: 'rgb(130, 124, 161)' }} ><FaCaretDown /></div>
+       </div>
     </div>
     </div>
    
