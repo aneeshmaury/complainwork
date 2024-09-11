63940627301607fa6db2fb12b3037e71a6fb5c73
aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-09-11 12:33:19 +0530

    sdfsd

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import { IoReorderThreeOutline } from "react-icons/io5";
 
 function App() {
   return (
-    <div>
-      <div className='font-bolder text-3xl'><IoReorderThreeOutline /></div>
-        <div className='font-bold' style={{ color: 'rgb(130, 124, 161)' }}>AK</div>
+    <div className='mt-7'>
+       <div className='flex'>
+      <div className='font-bolder text-3xl'style={{ color: 'rgb(130, 124, 161)' }} ><IoReorderThreeOutline /></div>
+        <div className='text-3xl bg-slate-200 rounded-full w-[50px] h-[50px] text-center ' style={{ color: 'rgb(130, 124, 161)' }}>AK</div>
     </div>
+    </div>
+   
   )
 }
 
