58256080ee7e3e9fe90413fead6137c566cc99e6
aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-09-11 13:52:01 +0530

    dfsdfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
        <div className='flex justify-between'>
       <div className='font-bolder text-[50px] text-slate-300' ><IoReorderThreeOutline /></div>
        <div className='flex items-center gap-2'>
-       <div className='text-2xl font-[2px] bg-slate-100 rounded-full w-[50px] h-[50px] flex items-center justify-center' style={{ color: 'rgb(130, 124, 161)' }}>
+       <div className='text-2xl font-[2px] bg-slate-100 rounded-full w-[40px] h-[40px] flex items-center justify-center' style={{ color: 'rgb(130, 124, 161)' }}>
   AK
 </div>
 <div className='2xl text-slate-300' ><FaCaretDown /></div>
