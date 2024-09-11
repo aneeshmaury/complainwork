394bba7655b52645d1ae4e82956f54d7a3d8f0a8
aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-09-11 13:58:02 +0530

    dfsdfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className='mt-7 px-3'>
        <div className='flex justify-between'>
-      <div className='font-bolder text-3xl text-slate-300' ><IoReorderThreeOutline /></div>
+      <div className='font-bolder text-[40px] text-slate-300' ><IoReorderThreeOutline /></div>
        <div className='flex items-center gap-2'>
        <div className='font-[1px] bg-slate-100 rounded-full w-[40px] h-[40px] flex items-center justify-center' style={{ color: 'rgb(130, 124, 161)' }}>
   AK
